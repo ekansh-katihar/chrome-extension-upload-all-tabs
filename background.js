2823191e18b56ea9e1c792d9d17d67836c832169
Ekansh <ekansh.katihar@gmail.com>
2023-09-20 00:20:46 -0400

    this will  upload all the tabs

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 // background.js
-const query = {};
-query.currentWindow = true;
-
-let tabs = await chrome.tabs.query(query);
-tabs.forEach(tab => console.log(tab));
+// const query = {};
+// query.currentWindow = true;
+//
+// let tabs = await chrome.tabs.query(query);
+// tabs.forEach(tab => console.log(tab));
 
 chrome.runtime.onInstalled.addListener(function () {
   console.log("Hello from background");
--- popup.js
+++ popup.js
@@ -9,16 +9,55 @@ changeColor.addEventListener("click", async () => {
 
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setBorderColor,
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => {
+          const bodyText = document.body.innerText.toLowerCase();
+          var title = document.getElementsByTagName('title')[0].text.replace(/ /g, "_")
+          var htmlElement = document.getElementsByTagName('html')[0]; // Assuming you want the first <html> element
+          var htmlString = htmlElement.outerHTML;
+          var url = 'https://vcq34knn47nwz77dnexcztoi6i0achue.lambda-url.us-east-2.on.aws/?title='+title;
+          // Create a new XMLHttpRequest object
+          var xhr = new XMLHttpRequest();
+
+          // Configure the POST request
+          xhr.open('POST', url, true);
+          xhr.setRequestHeader('Content-Type', 'application/json'); // Set the content type to JSON, adjust as needed
+
+          // Define a callback function to handle the response
+          xhr.onload = function () {
+            if (xhr.status === 200) {
+              // Request was successful
+              var responseData = JSON.parse(xhr.responseText);
+              console.log('POST request successful:', responseData);
+            } else {
+              // Request failed
+              console.error('POST request failed:', xhr.statusText);
+            }
+          };
+
+          // Handle network errors
+          xhr.onerror = function () {
+            console.error('Network error occurred');
+          };
+          // Send the POST request with the JSON data
+          xhr.send(htmlString); // Convert data to JSON format if needed
+        },
+        args: ["wordToSearch"],
+      });
+    });
   });
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   function: setBorderColor,
+  // });
 });
 
-function setBorderColor() {
-  var title = document.getElementsByTagName('title')[0].text.replace(/ /g, "_")
-  var htmlElement = document.getElementsByTagName('html')[0]; // Assuming you want the first <html> element
-  var htmlString = htmlElement.outerHTML;
+
+function uploadToS3(title, htmlString) {
+
   // console.log(htmlString);
 
   console.log('makePOSTRequest');
